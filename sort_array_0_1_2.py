ccc8b48ba5669b32f3ce5003b60a9c4d8c3e6c18
PRIYANGA-SELVAPERUMAL <132754349+PRIYANGA-SELVAPERUMAL@users.noreply.github.com>
2024-09-09 18:28:57 +0530

    Add files via upload

---
+++ sort_array_0_1_2.py
@@ -0,0 +1,18 @@
+def sort_array(arr):
+    low, mid, high = 0, 0, len(arr) - 1
+    
+    while mid <= high:
+        if arr[mid] == 0:
+            arr[low], arr[mid] = arr[mid], arr[low]
+            low += 1
+            mid += 1
+        elif arr[mid] == 1:
+            mid += 1
+        else:
+            arr[high], arr[mid] = arr[mid], arr[high]
+            high -= 1
+
+arr = [0, 1, 2, 1, 0, 2, 1, 0]
+sort_array(arr)
+print("Sorted array:", arr)
+
